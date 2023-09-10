97ea8c4ded199191aab404f250b7de7018c2bf96
rafaelapianezzer <rafaelapianezzer@outlook.com>
2023-09-10 14:06:58 -0300

    "Conclusão da tarefa 1"

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+
+    const fraseFormatada = str.toLowerCase().replaceAll(' ', '');
+    const frase = fraseFormatada.split('');
+    const fraseAoContrario = frase.reverse();
+    const fraseAoContrarioString = fraseAoContrario.join('');
+    
+
+    if (fraseFormatada === fraseAoContrarioString){
+        return true
+    } else {
+        return false
+    }
+  
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+
+
+// function arrayMaxMin(arr){
+//     /* Seu código aqui */
+// }
+
+
+
+
+
+
+
+
+
+
+
