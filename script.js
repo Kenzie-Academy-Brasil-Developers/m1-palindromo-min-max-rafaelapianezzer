72d42dabfa08c635824c1d2b5dc63efd6f0c5fc7
rafaelapianezzer <rafaelapianezzer@outlook.com>
2023-09-10 15:37:03 -0300

    "Finalização tarefa 2 - min/max"

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-function isPalindrome(str){
+function isPalindrome(str) {
 
     const fraseFormatada = str.toLowerCase().replaceAll(' ', '');
     const frase = fraseFormatada.split('');
     const fraseAoContrario = frase.reverse();
     const fraseAoContrarioString = fraseAoContrario.join('');
-    
 
-    if (fraseFormatada === fraseAoContrarioString){
+
+    if (fraseFormatada === fraseAoContrarioString) {
         return true
     } else {
         return false
     }
-  
+
 }
 
 
+function arrayMaxMin(arr) {
+    let max = 0;
+    let min = 0;
+    let resultado = [];
 
-// function arrayMaxMin(arr){
-//     /* Seu código aqui */
-// }
+    for (let i = 0; i < arr.length; i++) {
+        const numeroAtual = arr[i];
+        if (numeroAtual > max) {
+            max = numeroAtual
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        const numeroAtual = arr[i];
+        if (numeroAtual < min || min === 0){
+            min = numeroAtual
+        }
+    }
+
+
+    resultado[0] = min
+    resultado[1] = max
+    console.log(resultado)
+    return resultado
+}
 
 
 
